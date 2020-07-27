f87af8e65e8c01517f1a00c9c9540914d195aa79
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-07-27 19:32:53 +0430

    test is added to the project

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    transformIgnorePatterns: ['node_modules/(?!(react-dnd|dnd-core|react-dnd-html5-backend)/)'],
+    "moduleNameMapper": {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy"  }
+}

26e44f5fcf4bbfd4483015c022554c5392808e87
ebrahimsalehi1 <eeeee.bbbbb.iiiii@gmail.com>
2020-07-31 12:16:58 +0430

    Updates

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    transformIgnorePatterns: ['node_modules/(?!(react-dnd|dnd-core|react-dnd-html5-backend)/)'],
-    "moduleNameMapper": {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy"  }
-}

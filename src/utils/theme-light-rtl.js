6aada9db8ed0fa82afeb60b3656479b7555d558c
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-08-22 16:03:37 +0430

    some changes on themes, time: 1 h

--- src/utils/theme-light-rtl.js
+++ src/utils/theme-light-rtl.js
@@ -25,4 +25,22 @@ export const themeLightRTL = createMuiTheme({
             backgroundColor: '#3f51b5'
         }
     },
+    overrides: {
+
+    MuiCardHeader: {
+        root: {
+            padding: '8px'
+        }, title: {
+            fontSize: '0.75rem',
+            fontWeight: 'bold',
+            color: '#263238'
+        }, action: {
+            marginTop: 0,
+            color:'black'
+        }, avatar: {
+            marginRight: 9,
+            marginLeft: 0,
+        }
+    },
+    }
 }, locales['faIR']);
